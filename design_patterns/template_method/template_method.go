903ddc50894433ba0e4ee21ffdcf361049d7d760
y-mitsuyoshi <glorymanchesteru@gmail.com>
2023-10-24 09:40:58 +0900

    fix indent

--- design_patterns/template_method/template_method.go
+++ design_patterns/template_method/template_method.go
@@ -1,69 +1,69 @@
- package main
- 
- import "fmt"
- 
- // AbstractClass はテンプレートメソッドを持つ抽象クラスです。
- type IAbstractClass interface {
-     step1()
-     step2()
-     step3()
- }
- type AbstractClass struct {
-     iAbstractClass IAbstractClass
- }
- 
- func (c *AbstractClass) TemplateMethod() {
-     c.iAbstractClass.step1()
-     c.iAbstractClass.step2()
-     c.iAbstractClass.step3()
- }
- 
- // ConcreteClassA は AbstractClass の具象クラスです。
- type ConcreteClassA struct {
-     AbstractClass
- }
- 
- func (c *ConcreteClassA) step1() {
-     fmt.Println("ConcreteClassA: Step 1")
- }
- 
- func (c *ConcreteClassA) step2() {
-     fmt.Println("ConcreteClassA: Step 2")
- }
- 
- func (c *ConcreteClassA) step3() {
-     fmt.Println("ConcreteClassA: Step 3")
- }
- 
- // ConcreteClassB は AbstractClass の別の具象クラスです。
- type ConcreteClassB struct {
-     AbstractClass
- }
- 
- func (c *ConcreteClassB) step1() {
-     fmt.Println("ConcreteClassB: Step 1")
- }
- 
- func (c *ConcreteClassB) step2() {
-     fmt.Println("ConcreteClassB: Step 2")
- }
- 
- func (c *ConcreteClassB) step3() {
-     fmt.Println("ConcreteClassB: Step 3")
- }
- 
- func main() {
-     fmt.Println("Using ConcreteClassA:")
-     concreteA := &ConcreteClassA{}
-     a := AbstractClass{
-         iAbstractClass: concreteA,
-     }
-     a.TemplateMethod()
- 
-     fmt.Println("Using ConcreteClassB:")
-     concreteB := &ConcreteClassB{}
-     b := AbstractClass{
-         iAbstractClass: concreteB,
-     }
-     b.TemplateMethod()
- }
+package main
+
+import "fmt"
+
+// AbstractClass はテンプレートメソッドを持つ抽象クラスです。
+type IAbstractClass interface {
+	step1()
+	step2()
+	step3()
+}
+type AbstractClass struct {
+	iAbstractClass IAbstractClass
+}
+
+func (c *AbstractClass) TemplateMethod() {
+	c.iAbstractClass.step1()
+	c.iAbstractClass.step2()
+	c.iAbstractClass.step3()
+}
+
+// ConcreteClassA は AbstractClass の具象クラスです。
+type ConcreteClassA struct {
+	AbstractClass
+}
+
+func (c *ConcreteClassA) step1() {
+	fmt.Println("ConcreteClassA: Step 1")
+}
+
+func (c *ConcreteClassA) step2() {
+	fmt.Println("ConcreteClassA: Step 2")
+}
+
+func (c *ConcreteClassA) step3() {
+	fmt.Println("ConcreteClassA: Step 3")
+}
+
+// ConcreteClassB は AbstractClass の別の具象クラスです。
+type ConcreteClassB struct {
+	AbstractClass
+}
+
+func (c *ConcreteClassB) step1() {
+	fmt.Println("ConcreteClassB: Step 1")
+}
+
+func (c *ConcreteClassB) step2() {
+	fmt.Println("ConcreteClassB: Step 2")
+}
+
+func (c *ConcreteClassB) step3() {
+	fmt.Println("ConcreteClassB: Step 3")
+}
+
+func main() {
+	fmt.Println("Using ConcreteClassA:")
+	concreteA := &ConcreteClassA{}
+	a := AbstractClass{
+		iAbstractClass: concreteA,
+	}
+	a.TemplateMethod()
+
+	fmt.Println("Using ConcreteClassB:")
+	concreteB := &ConcreteClassB{}
+	b := AbstractClass{
+		iAbstractClass: concreteB,
+	}
+	b.TemplateMethod()
+}
